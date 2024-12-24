e9cf9bc5c2dbf74db86aa45736667313a391d959
harsh shivam <harsh.cse2021@ritroorkee.com>
2024-12-24 23:01:26 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors =require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+module.exports =app;
