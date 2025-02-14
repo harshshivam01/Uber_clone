39511ea697c34cc6a85e634e0e2a4c4b53fc207c
harsh shivam <harsh.cse2021@ritroorkee.com>
2025-02-14 20:49:14 +0530

    name error resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from "react-router-dom"
 import UserLogin from "./pages/UserLogin"
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import UserSignup from "./pages/UserSignup"
 import CaptainSignup from "./pages/CaptainSignup"
 import CaptainLogin from "./pages/CaptainLogin"
